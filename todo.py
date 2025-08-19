6cf693b1d776bebdc36c98a680024152eaacb87c
safae <safaebsila@gmail.com>
2025-08-19 08:40:29 +0200

    Ajout de la fonctionnalité : lister les tâches

--- todo.py
+++ todo.py
@@ -2,3 +2,14 @@ def ajouter_tache(tache):
     with open("todo.txt", "a") as f:
         f.write(tache + "\n")
     print(f"Tâche ajoutée : {tache}")
+def lister_taches():
+    try:
+        with open("todo.txt", "r") as f:
+            taches = f.readlines()
+        if not taches:
+            print("Aucune tâche pour l’instant ✅")
+        else:
+            for i, tache in enumerate(taches, 1):
+                print(f"{i}. {tache.strip()}")
+    except FileNotFoundError:
+        print("Pas encore de tâches.")
