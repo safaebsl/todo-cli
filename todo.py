5fb9f8e204044056e808f9305be867dd86a567ae
safae <safaebsila@gmail.com>
2025-08-19 08:46:09 +0200

    Ajout de la fonctionnalité : marquer une tâche comme faite

--- todo.py
+++ todo.py
@@ -13,3 +13,13 @@ def lister_taches():
                 print(f"{i}. {tache.strip()}")
     except FileNotFoundError:
         print("Pas encore de tâches.")
+def marquer_tache_faite(num):
+    with open("todo.txt", "r") as f:
+        taches = f.readlines()
+    if 0 < num <= len(taches):
+        taches[num-1] = "[X] " + taches[num-1]
+        with open("todo.txt", "w") as f:
+            f.writelines(taches)
+        print(f"Tâche {num} marquée comme faite ✅")
+    else:
+        print("Numéro de tâche invalide.")
